1e09c0e19992cdf711405a98feb5fb17f3e7291f
Prakhar <Prakharyadav9@gmail.com>
2023-01-31 17:56:29 +0530

    more BS questions

--- Striver-Sheet/src/com/py/sheet/binarysearch/easy/Q01_BinarySearch.java
+++ Striver-Sheet/src/com/py/sheet/binarysearch/easy/Q01_BinarySearch.java
@@ -36,7 +36,7 @@ public class Q01_BinarySearch {
 		return idx;
     }
 	public static void main(String[] args) {
-		int arr[]= {1,2,3,4,5,6,7,8,9};
+		int arr[]= {1,2,3,4,5,6,7,7,7,7,7,8,9};
 		int k=7;
 		System.out.println(binarysearch(arr, arr.length, k));
 		System.out.println(binarySearchRec(arr, 0, arr.length, k));
