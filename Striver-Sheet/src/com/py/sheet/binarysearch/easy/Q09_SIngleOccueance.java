b9b57db9af878d6d37d4aac4000ee8abdfee812d
Prakhar <Prakharyadav9@gmail.com>
2023-02-06 20:10:46 +0530

    more questions

---
+++ Striver-Sheet/src/com/py/sheet/binarysearch/easy/Q09_SIngleOccueance.java
@@ -0,0 +1,29 @@
+package com.py.sheet.binarysearch.easy;
+
+public class Q09_SIngleOccueance {
+
+	static int findOnce(int arr[], int n)
+    {
+        // Complete this function
+
+		int lo=0, hi=n-1, mid;
+        while(lo<hi) {
+            mid=(lo+hi)>>1;
+            if((mid & 1) !=0) 
+            	mid--;
+            if(arr[mid]==arr[mid+1])
+            	lo=mid+2;
+            else 
+            	hi=mid;
+        }
+        if(lo<n)
+        	return arr[lo];
+        return 0;
+    }
+	
+	public static void main(String[] args) {
+		int arr[] = {1, 2, 3, 3,-4, 50, 50, 65, 65};
+		System.out.println(findOnce(arr, arr.length));
+	}
+
+}
