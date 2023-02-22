4990f2d5683bd5a2d0db57c2e11eee93aeb3be71
Prakhar <Prakharyadav9@gmail.com>
2023-02-22 13:23:50 +0530

    More Questions

---
+++ Striver-Sheet/src/com/py/sheet/potd/gfg/POTD_02_21.java
@@ -0,0 +1,26 @@
+package com.py.sheet.potd.gfg;
+
+public class POTD_02_21 {
+
+	
+	public static int minIteration(int N, int M, int x, int y){
+		//code here
+		int top = x-1;
+		int down = N-x;
+		
+		int left = y-1;
+		int right = M-y;
+		int maxXdir = Math.max(left,right);
+		int maxYdir = Math.max(top,down);
+		return maxYdir+maxXdir;
+	} 
+	
+	public static void main(String[] args) {
+		int N= 1, M = 1;
+		int x= 1, y=1;
+		
+		System.out.printf("Minimum Iterations Required :: %d", minIteration(N, M, x, y));
+
+	}
+
+}
