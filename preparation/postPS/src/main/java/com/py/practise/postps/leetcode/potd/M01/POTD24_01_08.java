9aa9f67786c6c4541ee0fca9a90914c9941dc396
prakharyadav-9 <py@example.com>
2024-01-09 00:00:20 +0530

    potd

---
+++ preparation/postPS/src/main/java/com/py/practise/postps/leetcode/potd/M01/POTD24_01_08.java
@@ -0,0 +1,10 @@
+package com.py.practise.postps.leetcode.potd.M01;
+
+public class POTD24_01_08 {
+    /*
+     * URL: https://leetcode.com/problems/range-sum-of-bst/?envType=daily-question&envId=2024-01-08 
+     * 
+     * Given the root node of a binary search tree and two integers low and high,
+     * return the sum of values of all nodes with a value in the inclusive range [low, high].
+     */
+}
