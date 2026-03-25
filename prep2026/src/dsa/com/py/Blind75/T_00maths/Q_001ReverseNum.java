90b5ca93c25b54fff2463356b8905671311ad9e0
prakharyadav-9 <py@example.com>
2026-03-25 23:57:39 +0530

    chore: restructured class

--- prep2026/src/dsa/com/py/Blind75/T_00maths/Q_001ReverseNum.java
+++ prep2026/src/dsa/com/py/Blind75/T_00maths/Q_001ReverseNum.java
@@ -4,6 +4,7 @@ package dsa.com.py.Blind75.T_00maths;
  * Problem link:<a> https://takeuforward.org/plus/dsa/problems/reverse-a-number?source=strivers-a2z-dsa-track </a>
  */
 public class Q_001ReverseNum {
+    
     public int reverse(int n) {
         long rev =0;
         while(n!=0){
